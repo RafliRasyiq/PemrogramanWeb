2aeeb41c18ab0ff7446c9b79c56d3a19cad4a561
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-02-26 12:08:51 +0700

    pertama kali

---
+++ Pertemuan_2/variable.php
@@ -0,0 +1,14 @@
+<?php
+$name="Rafli";
+$alamat="Kepanjen";
+define("Gaji", 5000000000);
+define("nomorRumah", 07);
+
+
+echo $name;
+echo "<br>";
+echo Gaji;
+echo "<br>";
+echo "Nama saya : ".$name;
+echo "<br>";
+?>
