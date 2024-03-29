fd2de973686e0bdc9c860da933cb0d5e304e76e1
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-03-29 16:23:08 +0700

    Pratikum 2

---
+++ Pertemuan_8/proses_upload.php
@@ -0,0 +1,33 @@
+<?php
+// lokasi penyimpanan file yang diunggah
+    $targetDirectory = "documents/";
+
+    // periksa apakah direktori penyimpanan ada, jika tidak maka buat
+    if (!file_exists($targetDirectory)) { 
+        mkdir($targetDirectory, 0777, true);
+    }
+
+    if ($_FILES['files']['name'][0]) {
+        $totalFiles = count($_FILES['files']['name']); 
+
+        // loop melalui semua file yang diunggah
+        for ($i = 0; $i < $totalFiles; $i++) {
+            $fileName = $_FILES['files']['name'][$i]; 
+            $targetFile = $targetDirectory . $fileName;
+
+            // pindahkan file yang diunggah ke direktori penyimpanan
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>"; 
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
+        }
+    } else {
+        echo "Tidak ada file yang diunggah.";  
+    }
+
+    /* SOAL 2.1
+    Simpan file tersebut, kemudian buka browser dan jalankan localhost/dasarWeb/ form_multiupload.php. 
+    Apa yang anda pahami dari script pada file tersebut. Catat di bawah ini pemahaman anda. */
+    // 
+?>
