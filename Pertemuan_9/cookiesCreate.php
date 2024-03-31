2d387c2f3e628e5feb83c10c7bc8042cac1eb9bb
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-03-31 22:34:22 +0700

    Praktikum 5

---
+++ Pertemuan_9/cookiesCreate.php
@@ -0,0 +1,9 @@
+<?php
+    setcookie("user", "Polinema",time()+3600);
+?>
+
+<!-- SOAL 12 -->
+<!-- Maka akan muncul Polinema -->
+
+<!-- SOAL 13 -->
+<!-- terdapat error karena cookies ter reset -->
