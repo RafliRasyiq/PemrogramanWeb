c4303cae696f5be15c0dacdb2bd0488820c26b13
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-03-31 23:50:26 +0700

    Praktikum 5

--- Pertemuan_10/crud/create.php
+++ Pertemuan_10/crud/create.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Anggota</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="container">
+    <!-- <div class="container">
         <h2>Tambah Data Anggota</h2>
         <form action="proses.php?aksi=tambah" method="post">
             <label for="nama">Nama:</label>
@@ -33,7 +34,41 @@
             <button type="submit">Simpan Data</button>
             <a href="index.php" class="btn-kembali">Kembali</a>
         </form>
+    </div> -->
+
+    <div class="container mt-4">
+        <h2>Tambah data anggota</h2>
+        <form action="proses.php?aksi=tambah" method="post">
+            <div class="form-group">
+                <label for="nama">Nama : </label>
+                <input type="text" class="form-control" name="nama" id="nama" required>
+            </div>
+            <div class="form-group">
+                <label for="jenis_kelamin">Jenis kelamin : </label>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" id="laki" required>
+                    <label class="form-check-label" for="laki">Laki-laki</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="P" id="perempuan" required>
+                    <label class="form-check-label" for="perempuan">Perempuan</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat : </label>
+                <input type="text" class="form-control" name="alamat" id="alamat" required>
+            </div>
+            <div class="form-group">
+                <label for="no_telp">No telp : </label>
+                <input type="text" class="form-control" name="no_telp" id="no_telp" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan data</button>
+        </form>
+        <a class="btn btn-secondary" href="index.php">Kembali</a>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
